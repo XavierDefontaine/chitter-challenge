3361b2983c66e04a8e6cd62c33d993367717f78d
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-26 15:56:23 +0000

    Added #all method returning all peeps

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,15 +3,22 @@ require './lib/database_connection_setup'
 
 class Peep
 
-  attr_reader :text
+  attr_reader :id, :text, :date_time
 
-  def initialize(text:)
+  def initialize(id:, text:, date_time:)
+    @id = id
     @text = text
+    @date_time = date_time
   end
 
   def self.add(text:)
-    peep = DBconnect.query("INSERT INTO peeps (text) VALUES('#{text}') RETURNING text;")
-    Peep.new(text: peep[0]['text'])
+    peep = DBconnect.query("INSERT INTO peeps (text) VALUES('#{text}') RETURNING id, text, date_time;")
+    Peep.new(id: peep[0]['id'], text: peep[0]['text'], date_time: peep[0]['date_time'])
+  end
+
+  def self.all
+    peeps = DBconnect.query("SELECT * FROM peeps;")
+    peeps.map{|peep| Peep.new(id: peep['id'], text: peep['text'], date_time: peep['date_time'])}
   end
 
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,10 +2,21 @@ require './lib/peep'
 
 describe Peep do
   let(:peep) { described_class.new }
+
   describe '#add' do
     it 'returns its text' do
       peep = Peep.add(text: "hello there")
       expect(peep.text).to eq "hello there"
+    end
+  end
+
+  describe '#all' do
+    it 'returns all peeps' do
+      peep1 = Peep.add(text: "I am first!")
+      peep2 = Peep.add(text: "I am second!")
+      peeps = Peep.all
+      expect(peeps.length).to eq 2
+      expect(peeps.first.text).to eq "I am first!"
     end
   end
 
