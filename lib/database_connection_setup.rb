0d986aa465fd00033fdfd3ae1da55fff733223d9
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-26 11:22:19 +0000

    first commit

--- lib/database_connection_setup.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'database_connection'
-
-if ENV['ENVIRONMENT'] == 'test'
-  DBconnect.setup('chitter_test') 
-else
-  DBconnect.setup('chitter')
-end
--- spec/database_helpers.rb
+++
@@ -1,7 +0,0 @@
-
-
-def add_peeps
-  Peep.create(text: 'Otters are cute')
-  Peep.create(text: 'But Squirels are better')
-  Peep.create(text: 'Yo wadddup')
-end
